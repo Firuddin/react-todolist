cf1a5f65a85be1fd979c22e6610b79ef44878e8f
Firuddin <firuddinaliyev888@gmail.com>
2023-11-20 12:53:28 +0400

    add new useState

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -2,6 +2,22 @@ import React, {useState} from "react"
 import "./app.css"
 
 function App(){
+  const[newItem,setNewItem]=useState("")
+  const[items,setitem]=useState([])
+
+function addItem(){
+  if(!newItem){
+    alert("enter an items")
+    return;
+  }
+  const item={
+    id:Math.floor(Math.random()*1000),
+    value:newItem
+  }
+  setitem(olditems=>[...olditems,item])
+  setNewItem("");
+}
+
   return(
     <div className="app">
 <h1>todo list app</h1>
@@ -10,12 +26,18 @@ function App(){
 <input 
 type="text"
 placeholder="add an item..." 
+value={newItem}
+onChange={e=>setNewItem(e.target.value)}
 />
-<button>Add</button>
+<button onClick={()=>addItem()}>Add</button>
 <ul>
-  <li></li>
-  <li></li>
-  <li></li>
+  {items.map(item=>{
+    return(
+<li key={item.id}>{item.value}</li>
+
+    )
+    })}
+
 </ul>
     </div>
   )
